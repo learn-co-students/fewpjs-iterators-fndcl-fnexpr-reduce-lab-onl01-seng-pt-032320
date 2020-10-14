3af1b603af6e958ad80b15a92139b2a22cbffd41
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-14 03:30:07 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// function customReducer(total, element) { return total + element; }
+const customReducer = (total, element) => total + element; 
+
+let totalBatteries = batteryBatches.reduce(customReducer, 0);
